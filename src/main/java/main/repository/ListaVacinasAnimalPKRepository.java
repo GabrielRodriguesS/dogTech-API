3fe9f5af787970cb8b424ac85ea685185d2ad8bd
Gabriel Rodrigues de Souza <rodriguez.gabriel001@gmail.com>
2018-03-13 18:55:00 -0300

    Bugs from files generated solved. Question: its necessary many repositories files or its is valid create one generic repository class?

--- src/main/java/main/repository/ListaVacinasAnimalPKRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import main.model.ListaVacinasAnimalPK;
-
-public interface ListaVacinasAnimalPKRepository extends JpaRepository<ListaVacinasAnimalPK, Long> {
-
-}
--- src/main/java/main/repository/ServicosPersonPKRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import main.model.ServicosPersonPK;
-
-public interface ServicosPersonPKRepository extends JpaRepository<ServicosPersonPK, Long> {
-
-}
