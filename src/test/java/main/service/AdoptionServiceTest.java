337fba94db1fc965f91f00efa1462fa97d92ea81
Gabriel Rodrigues de Souza <rodriguez.gabriel001@gmail.com>
2018-06-03 14:04:17 -0300

    Mexi nos testes, apenas...

--- src/test/java/main/service/AdoptionServiceTest.java
+++ src/test/java/main/service/AdoptionServiceTest.java
@@ -1,10 +1,12 @@
 package main.service;
 
 import main.DogTechApiApplication;
+import main.domain.dto.AdoptionDTO;
 import main.domain.model.Adoption;
 import main.domain.model.Animal;
 import main.domain.repository.AdoptionRepository;
 import main.domain.stateMachine.stateMachineEnums.States;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +21,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -39,16 +42,21 @@ public class AdoptionServiceTest {
         now = ZonedDateTime.now();
     }
 
+    @After
+    public void tearDown() {
+        Mockito.reset(this.adoptionRepository);
+    }
+
     @Test
     public void testAdoptionsWithoutMoreThanFifteenDays() {
         Mockito.when(this.adoptionRepository.findAdoptionsByStatusIsAndPostAdoptionListIsNull(States.ADOPTED))
                 .thenReturn(new ArrayList<Adoption>() {{
-                    add(new Adoption(Date.from(now.plusDays(-1).toInstant()), new Animal("a1")));
-                    add(new Adoption(Date.from(now.plusMonths(1).toInstant()), new Animal("a2")));
-                    add(new Adoption(Date.from(now.plusDays(-14).toInstant()), new Animal("a5")));
-                    add(new Adoption(Date.from(now.plusDays(15).toInstant()), new Animal("a3")));
-                    add(new Adoption(Date.from(now.plusYears(1).toInstant()), new Animal("a4")));
-                    add(new Adoption(Date.from(now.plusMonths(1).toInstant()), new Animal("a6")));
+                    add(new Adoption(Date.from(now.plusDays(-1).toInstant()), new Animal("one day ago")));
+                    add(new Adoption(Date.from(now.plusMonths(1).toInstant()), new Animal("one month on future")));
+                    add(new Adoption(Date.from(now.plusDays(-14).toInstant()), new Animal("14 days ago")));
+                    add(new Adoption(Date.from(now.plusDays(15).toInstant()), new Animal("15 days on future")));
+                    add(new Adoption(Date.from(now.plusYears(1).toInstant()), new Animal("one year on future")));
+                    add(new Adoption(Date.from(now.plusMonths(1).toInstant()), new Animal("one month on future")));
                 }});
         assertEquals(0, this.adoptionService.findAdoptionsToRequestFeedback().size());
     }
@@ -57,23 +65,25 @@ public class AdoptionServiceTest {
     public void testAdoptionsWithMoreThanFifteenDays() {
         Mockito.when(this.adoptionRepository.findAdoptionsByStatusIsAndPostAdoptionListIsNull(States.ADOPTED))
                 .thenReturn(new ArrayList<Adoption>() {{
-                    add(new Adoption(Date.from(now.plusDays(-15).toInstant()), new Animal("a1")));
-                    add(new Adoption(Date.from(now.plusDays(-16).toInstant()), new Animal("a2")));
-                    add(new Adoption(Date.from(now.plusMonths(-1).toInstant()), new Animal("a5")));
-                    add(new Adoption(Date.from(now.plusYears(-1).toInstant()), new Animal("a3")));
+                    add(new Adoption(Date.from(now.plusDays(-15).toInstant()), new Animal("15 days ago")));
+                    add(new Adoption(Date.from(now.plusDays(-16).toInstant()), new Animal("16 days ago")));
+                    add(new Adoption(Date.from(now.plusMonths(-1).toInstant()), new Animal("1 month ago")));
+                    add(new Adoption(Date.from(now.plusYears(-1).toInstant()), new Animal("1 year ago")));
                 }});
-        assertEquals(4, this.adoptionService.findAdoptionsToRequestFeedback().size());
+        List<AdoptionDTO> listDTO = this.adoptionService.findAdoptionsToRequestFeedback();
+        listDTO.stream().forEach(adoptionDTO -> System.out.println(adoptionDTO.getAnimalName()));
+        assertEquals(4, listDTO.size());
     }
 
     @Test
     public void testAdoptionsMixed() {
         Mockito.when(this.adoptionRepository.findAdoptionsByStatusIsAndPostAdoptionListIsNull(States.ADOPTED))
                 .thenReturn(new ArrayList<Adoption>() {{
-                    add(new Adoption(Date.from(now.plusDays(-10).toInstant()), new Animal("a1")));  //to not return
-                    add(new Adoption(Date.from(now.plusDays(1).toInstant()), new Animal("a2"))); //to not return
-                    add(new Adoption(Date.from(now.plusDays(10).toInstant()), new Animal("a5"))); //to not return
-                    add(new Adoption(Date.from(now.plusDays(-16).toInstant()), new Animal("a3"))); //to return
-                    add(new Adoption(Date.from(now.plusDays(-20).toInstant()), new Animal("a4"))); //to return
+                    add(new Adoption(Date.from(now.plusDays(-10).toInstant()), new Animal("10 days ago")));  //to not return
+                    add(new Adoption(Date.from(now.plusDays(1).toInstant()), new Animal("one day in the future"))); //to not return
+                    add(new Adoption(Date.from(now.plusDays(10).toInstant()), new Animal("10 days in the future"))); //to not return
+                    add(new Adoption(Date.from(now.plusDays(-16).toInstant()), new Animal("16 days ago"))); //to return
+                    add(new Adoption(Date.from(now.plusDays(-20).toInstant()), new Animal("20 days ago"))); //to return
                 }});
         assertEquals(2, this.adoptionService.findAdoptionsToRequestFeedback().size());
     }
